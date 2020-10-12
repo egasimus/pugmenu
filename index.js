9b052ddf9a30edcd183372f825b50deb91ac5188
Adam Avramov <my.new@email.here>
2020-10-12 02:02:21 +0200

    works!

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
+
 const GI = require('node-gtk')
 GI.startLoop()
+
 const GTK = GI.require('Gtk', '3.0')
 GTK.init()
-const run = require('./src/UI')(GTK)
+
+const run = require('./src/UIBuilder')(GTK)
 run()
--- src/UIBuilder.js
+++ src/UIBuilder.js
@@ -1,70 +1,21 @@
 const assert = require('assert')
+const load = require('pug-load').string
+const lex = require('pug-lexer')
+const parse = require('pug-parser')
 
 module.exports = makeUI
 
 function makeUI (GTK) {
 
-  const menuSpecs = (function trim ({nodes}) {
-    return nodes.map(({name,attrs,block,val})=>
-      block ? {
-        type: name,
-        ...attrs.reduce(
-          (attrs,{name,val})=>Object.assign(attrs,{[name]:val}),
-          {}
-        ),
-        kids: trim(block)
-      } : val
-    )
-  })(require('pug-load').string(
-    require('fs').readFileSync(`${__dirname}/menu.pug`, 'utf8'),
-    { lex: require('pug-lexer'), parse: require('pug-parser') }
-  ))
+  const config = require('fs').readFileSync(`${__dirname}/menu.pug`, 'utf8')
+  const specs = parseConfig(config)
+  const build = getBuilder(GTK)
 
-  for (let menuSpec of menuSpecs) {
+  for (let menuSpec of specs) {
     console.log(require('util').inspect(menuSpec, { depth: Infinity }))
-    instantiate(menuSpec).showAll()
-  }
-
-  function instantiate ({type, kids, ...args}, parent = null) {
-    if (!parent) {
-      console.log({type,kids,args})
-      assert(type === 'Menu', 'only Menu can be root element')
-      assert(args.icon, 'root Menu must have icon attribute')
-      const icon = new GTK.StatusIcon.newFromFile(JSON.parse(args.icon))
-      const menu = new GTK.Menu()
-      icon.connect('popup-menu', (button, time) => {
-        console.log('showMenu', button, time)
-        menu.popup(
-          null, null,
-          (menu, icon) => GTK.StatusIcon.positionMenu(menu, 0, 0, icon),
-          button, time
-        )
-      })
-      for (let kid of kids) {
-        assert(typeof kid === 'object', "root children can't be strings")
-        instantiate(kid, menu)
-      }
-    } else {
-      const label = kids[0]
-      kids = kids.slice(1)
-      const item = new GTK.MenuItem()
-      switch (type) {
-        case 'Menu':
-          const menu = new GTK.Menu()
-          item.setSubmenu(menu)
-          for (let kid of kids) instantiate(kid, menu)
-          break
-        case 'Run':
-          const {command} = args
-          item.on('clicked', ()=>console.log(command))
-          break
-        default:
-          throw new Error(`unknown item type ${type}`)
-      }
-      item.setLabel(String(kids[0]))
-      parent.append(item)
-      return item
-    }
+    const menu = build(menuSpec)
+    console.log(menu)
+    menu[1].showAll()
   }
 
   return function run () {
@@ -73,3 +24,94 @@ function makeUI (GTK) {
 
 }
 
+
+function parseConfig (source) {
+
+  return walk(load(source, { lex, parse }))
+
+  function walk ({nodes}) {
+    const add = (obj,{name,val})=>Object.assign(obj,{[name]:val})
+    return nodes.map(({name,attrs,block,val})=>
+      block ? {
+        ...attrs.reduce(add, {}),
+        type: name,
+        kids: walk(block)
+      } : val
+    )
+  }
+
+}
+
+function getBuilder (GTK) {
+
+  return build
+
+  function build ({type, kids, ...args}, parent = null) {
+
+    if (!parent) {
+      assert(type === 'Menu', 'only Menu can be root element')
+      assert(args.icon, 'root Menu must have icon attribute')
+      return buildRoot(type, kids, args)
+    } else {
+      return buildItem(type, kids, args, parent)
+    }
+
+  }
+
+  function buildRoot (type, kids, args) {
+
+    console.debug('buildRoot', {type,kids,args})
+
+    const icon = new GTK.StatusIcon.newFromFile(JSON.parse(args.icon))
+    const menu = new GTK.Menu()
+    icon.connect('popup-menu', show)
+
+    return [icon, menu, ...kids.map(kid=>{
+      assert(typeof kid === 'object', "children of root can't be strings")
+      const item = build(kid, true)
+      menu.append(item)
+      return item
+    })]
+
+    function show (button, time) {
+      console.log('showMenu', button, time)
+      const position = menu => GTK.StatusIcon.positionMenu(menu, 0, 0, icon)
+      menu.popup(null, null, position, button, time)
+    }
+
+  }
+
+  function buildItem (type, kids, args) {
+
+    const label = String(args.label)
+    console.log('buildItem', {type,label,kids,args})
+
+    const item = new GTK.MenuItem()
+    item.setLabel(label)
+
+    switch (type) {
+      case 'Menu':
+        const menu = new GTK.Menu()
+        item.setSubmenu(menu)
+        for (let kid of kids) menu.append(build(kid, true))
+        break
+      case 'Run':
+        const {command} = args
+        item.on('activate', ()=>{
+          console.log('Run', kids, args)
+          require('child_process').execFile('sh', ['-c', kids.join('\n')], { stdio: 'inherit'})
+        })
+        break
+      case 'Open':
+        item.on('activate', ()=>{
+          console.debug('Open', kids, args)
+          require('child_process').execFile('xdg-open', kids, { stdio: 'inherit'})
+        })
+        break
+      default:
+        throw new Error(`unknown item type ${type}`)
+    }
+    return item
+  }
+
+}
